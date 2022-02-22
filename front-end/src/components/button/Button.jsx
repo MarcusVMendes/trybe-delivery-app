339a0b64c0170dc5dc1b6916534abc584b62b2bc
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-02-22 19:01:25 -0300

    Cria componente Button

---
+++ front-end/src/components/button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button({ text, testId }) {
+  return(
+    <button type="button" data-test-id={ testId }>
+      { text }
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
+};
+
+export default Button;
