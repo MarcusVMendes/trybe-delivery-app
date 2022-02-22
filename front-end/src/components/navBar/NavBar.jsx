5ef5f7cb806052c1e52773aa4de950a05a8efe1d
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-02-22 19:03:21 -0300

    Cria componente NavBar

---
+++ front-end/src/components/navBar/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NavBar({ ...links, userName, role }) {
+  const isCustomer = role === 'customer';
+  const baseUrl = 'http://localhost:3000/';
+
+  return(
+    <header>
+      <nav>
+        <div>
+          <a href={ ...links }>{ ...links }</a>
+          {isCustomer && <a href={ ...links }>{ ...links }</a>}
+        </div>
+        <div>
+          <p>{ userName }</p>
+          <a href={`${baseUrl}login`}>SAIR</a>
+        </div>
+      </nav>
+    </header>
+
+  );
+}
+
+NavBar.propTypes = {
+  links: PropTypes.arrayOf(PropTypes.object).isRequired,
+  userName: PropTypes.string.isRequired,
+};
+
+export default NavBar;
