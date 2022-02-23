2610abdf0ef631dad79ffd29fae300e9f9c3d8ad
MarcusVMendes <marcus.mendes89@gmail.com>
2022-02-23 18:00:08 -0300

    Adiciona a conversao para md5 password usuario

--- back-end/src/api/service/user.js
+++ back-end/src/api/service/user.js
@@ -1,4 +1,5 @@
 const { loginSchema } = require('../utils/validation');
+const md5 = require('crypto-js/md5');
 const { User } = require('../../database/models');
 const jwt = require('jsonwebtoken');
 const { BAD_REQUEST, BAD_REQUEST_MSG } = require('../utils/dictionary');
@@ -13,7 +14,9 @@ const getUserLoginService = async (email, password) => {
   const { error } = loginSchema.validate({ email, password });
   if (error) throw error;
   const user = await User.findOne({ where: { email } });
-  if (!user || user.dataValues.password !== password) {
+  const hashPassword = md5(password).toString();
+  console.log(hashPassword);
+  if (!user || user.dataValues.password !== hashPassword) {
     const errorUser = { code: BAD_REQUEST, message: BAD_REQUEST_MSG }
     throw errorUser;
   }
