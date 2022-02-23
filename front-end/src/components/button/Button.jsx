3a35e5fdcfddc2e40350f0d2c2a68ef1ea2f9bad
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-02-23 19:15:02 -0300

    Adiciona props action ao componente Button.jsx

--- front-end/src/components/button/Button.jsx
+++ front-end/src/components/button/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ text, testId, isDisabled }) {
+function Button({ text, testId, isDisabled, action }) {
   return (
-    <button type="button" data-testid={ testId } disabled={ isDisabled }>
+    <button
+      type="button"
+      data-testid={ testId }
+      disabled={ isDisabled }
+      onClick={ action }
+    >
       { text }
     </button>
   );
@@ -13,6 +18,7 @@ Button.propTypes = {
   text: PropTypes.string.isRequired,
   testId: PropTypes.string.isRequired,
   isDisabled: PropTypes.bool.isRequired,
+  action: PropTypes.func.isRequired,
 };
 
 export default Button;
