a3e2c368b81a17b0c573c5989be8027cd1915380
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-24 20:41:45 +0100

    Adiciona props name no input

--- front-end/src/components/input/Input.jsx
+++ front-end/src/components/input/Input.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Input.css';
-
-function Input({ label, type, placeholder, testId, handleChange }) {
+import React from "react";
+import PropTypes from "prop-types";
+import "./Input.css";
+function Input({ label, name, type, placeholder, testId, handleChange }) {
   return (
     <div className="input-container">
-      <label
-        className="input-label"
-        htmlFor={ `${label}-input` }
-      >
-        { label }
+      <label className="input-label" htmlFor={`${label}-input`}>
+        {label}
       </label>
       <input
-        id={ `${label}-input` }
+        id={`${label}-input`}
+        name={name}
         className="input"
-        type={ type }
-        placeholder={ placeholder }
+        type={type}
+        placeholder={placeholder}
         required
-        data-testid={ testId }
-        onChange={ handleChange }
+        data-testid={testId}
+        onChange={handleChange}
         autoComplete="off"
       />
     </div>
