0a20cd58efd49813978ab0140a3644ee2db53ca4
MarcusVMendes <marcus.mendes89@gmail.com>
2022-02-24 15:30:58 -0300

    Configura cors localhost na porta 3000

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const userRouter = require('./routes/user');
 const errorMiddleware = require('./middlewares/errorMidleware');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.use(express.json());
 
