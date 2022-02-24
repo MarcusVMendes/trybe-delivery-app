19ea16719a50f957332e1f0845e42ec73f2edb5d
MarcusVMendes <marcus.mendes89@gmail.com>
2022-02-24 15:34:48 -0300

    Resolve lint cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routes/user');
 const errorMiddleware = require('./middlewares/errorMidleware');
-const cors = require('cors');
 
 const app = express();
 
