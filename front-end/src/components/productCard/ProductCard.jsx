5aae9bedc86353cf83f87166bed13251437d63bf
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-02-25 16:06:50 -0300

    Adiciona props ao Componente ProductCard e css

--- front-end/src/components/productCard/ProductCard.jsx
+++ front-end/src/components/productCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Input from '../input/Input';
 import Button from '../button/Button';
 
-function Card({ productPrice, imageUrl, productName, id }) {
+function Card({ productPrice, imageUrl, productName, id, action }) {
   return (
     <div className="card">
       <span data-testid={ `customer_products__element-card-price-${id}` }>
@@ -17,13 +17,22 @@ function Card({ productPrice, imageUrl, productName, id }) {
       <span data-testid={ `customer_products__element-card-title-${id}` }>
         { productName }
       </span>
-      <Button testId={ `customer_products__button-card-rm-item-${id}` } text="-" />
+      <Button
+        testId={ `customer_products__button-card-rm-item-${id}` }
+        text="-"
+        action={ action }
+      />
       <Input
+        label=""
         testId={ `customer_products__input-card-quantity-${id}` }
-        type="number"
+        type="text"
         placeholder="0"
       />
-      <Button testId={ `customer_products__button-card-add-item-${id}` } text="+" />
+      <Button
+        testId={ `customer_products__button-card-add-item-${id}` }
+        text="+"
+        action={ action }
+      />
     </div>
   );
 }
@@ -33,6 +42,7 @@ Card.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   productName: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  action: PropTypes.func.isRequired,
 };
 
 export default Card;
