b98831d6b730531676a7ffe3797b6010e39dfb7d
Letícia Castro <aicitelks@gmail.com>
2022-02-27 11:41:56 -0300

    routes/sale.js: Correção lint

--- back-end/src/api/routes/sale.js
+++ back-end/src/api/routes/sale.js
@@ -1,8 +1,8 @@
+const router = require('express').Router();
+
 const auth = require('../utils/auth');
 
 const { createSaleController } = require('../controller/sale');
-
-const router = require('express').Router();
 
 router.post('/', auth, createSaleController);
 
