cbff2d47a5eb1a3a87301c3005471c1c69b635b0
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-28 22:47:28 +0100

    Adiciona o post do registro

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -9,6 +9,15 @@ const login = async (email, password) => {
   return data;
 };
 
+const register = async (name, email, password, role) => {
+  const body = { name, email, password, role };
+  const data = await axios.post(`${baseUrl}/register`, body);
+  const response = await data.json();
+
+  return response;
+};
+
 export default {
   login,
+  register,
 };
