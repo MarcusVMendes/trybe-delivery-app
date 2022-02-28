8ee54a232bae83057d086aade2610a691259fdec
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-28 22:45:57 +0100

    Arruma erros de lint no input

--- front-end/src/components/input/Input.jsx
+++ front-end/src/components/input/Input.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./Input.css";
-function Input({ label, name, type, placeholder, testId, handleChange }) {
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Input.css';
+
+function Input({ label, name, type, placeholder, testId, handleChange, onBlur }) {
   return (
     <div className="input-container">
-      <label className="input-label" htmlFor={`${label}-input`}>
+      <label className="input-label" htmlFor={ `${label}-input` }>
         {label}
       </label>
       <input
-        id={`${label}-input`}
-        name={name}
+        id={ `${label}-input` }
+        name={ name }
         className="input"
-        type={type}
-        placeholder={placeholder}
+        type={ type }
+        placeholder={ placeholder }
         required
-        data-testid={testId}
-        onChange={handleChange}
+        data-testid={ testId }
+        onChange={ handleChange }
         autoComplete="off"
+        onBlur={ onBlur }
       />
     </div>
   );
@@ -24,10 +26,17 @@ function Input({ label, name, type, placeholder, testId, handleChange }) {
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
+  name: PropTypes.string,
   placeholder: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   testId: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+};
+
+Input.defaultProps = {
+  name: '',
+  onBlur: () => {},
 };
 
 export default Input;
