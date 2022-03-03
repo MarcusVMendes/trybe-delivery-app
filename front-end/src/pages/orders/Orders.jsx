b5ebe585399d74702004a4e64f47bb0d63223ffe
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-03-03 19:21:21 -0300

    Inicia implementação da página de pedidos do cliente

---
+++ front-end/src/pages/orders/Orders.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import NavBar from '../../components/navBar/NavBar';
+import OrderCard from '../../components/orderCard/OrderCard';
+
+function Orders() {
+  const { name, role } = JSON.parse(localStorage.getItem('user'));
+  const links = [
+    {
+      name: 'PRODUTOS',
+      url: 'http://localhost:3000/customer/products',
+    },
+    {
+      name: 'MEUS PEDIDOS',
+      url: 'http://localhost:3000/customer/orders',
+    },
+  ];
+  const orders = [
+    {
+      id: 1,
+      orderNumber: '001',
+      status: 'PENDENTE',
+      date: '01/01/2022',
+      price: '23,80',
+    },
+    {
+      id: 2,
+      orderNumber: '002',
+      status: 'PREPARANDO',
+      date: '02/01/2022',
+      price: '14,20',
+    },
+    {
+      id: 3,
+      orderNumber: '003',
+      status: 'ENTREGUE',
+      date: '03/01/2022',
+      price: '28,46',
+    },
+  ];
+
+  return (
+    <>
+      <NavBar
+        userName={ name }
+        role={ role }
+        links={ links }
+      />
+      {
+        orders.map(({ orderNumber, status, date, price, id }) => (
+          <OrderCard
+            key={ orderNumber }
+            orderNumber={ orderNumber }
+            status={ status }
+            date={ date }
+            price={ price }
+            id={ id }
+          />
+        ))
+      }
+    </>
+  );
+}
+
+export default Orders;
