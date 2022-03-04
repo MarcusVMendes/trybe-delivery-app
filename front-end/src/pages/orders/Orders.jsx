6af88a12a79011723a9a60759d84a5f071acb806
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-03-04 17:10:16 -0300

    Adiciona função que retorna a vendas existentes no BD

--- front-end/src/pages/orders/Orders.jsx
+++ front-end/src/pages/orders/Orders.jsx
@@ -1,57 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Orders.css';
+import api from '../../services/api';
+import { customerLinks } from '../../utils/navBarLinks';
 import NavBar from '../../components/navBar/NavBar';
 import OrderCard from '../../components/orderCard/OrderCard';
 
 function Orders() {
-  const { name, role } = JSON.parse(localStorage.getItem('user'));
-  const links = [
-    {
-      name: 'PRODUTOS',
-      url: 'http://localhost:3000/customer/products',
-    },
-    {
-      name: 'MEUS PEDIDOS',
-      url: 'http://localhost:3000/customer/orders',
-    },
-  ];
+  const [order, setOrders] = useState([]);
+  const user = JSON.parse(localStorage.getItem('user'));
   const orders = [
     {
       id: 1,
-      orderNumber: '001',
       status: 'PENDENTE',
       date: '01/01/2022',
       price: '23,80',
     },
     {
       id: 2,
-      orderNumber: '002',
       status: 'PREPARANDO',
       date: '02/01/2022',
       price: '14,20',
     },
     {
       id: 3,
-      orderNumber: '003',
       status: 'ENTREGUE',
       date: '03/01/2022',
       price: '28,46',
     },
   ];
 
+  useEffect(() => {
+    const fetchSales = async () => {
+      try {
+        const response = await api.getSales(token);
+        const userOrders = response.filter((sale) => sale.id === user.id);
+
+        setOrders(userOrders);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSales();
+  });
+
   return (
     <>
       <NavBar
-        userName={ name }
-        role={ role }
-        links={ links }
+        userName={ user.name }
+        role={ user.role }
+        links={ customerLinks }
       />
       <div className="orders-container">
         {
-          orders.map(({ orderNumber, status, date, price, id }) => (
+          orders.map(({ id, status, date, price }) => (
             <OrderCard
-              key={ orderNumber }
-              orderNumber={ orderNumber }
+              key={ id }
+              orderNumber={ id }
               status={ status }
               date={ date }
               price={ price }
