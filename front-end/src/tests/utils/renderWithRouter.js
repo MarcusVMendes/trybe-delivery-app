7bd89224e94acfd83ce2dea69be79f2bcbe03d52
Letícia Castro <aicitelks@gmail.com>
2022-03-05 10:32:03 -0300

    Implementado o renderWithRouter

---
+++ front-end/src/tests/utils/renderWithRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router } from 'react-router';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+
+function renderWithRouter(componentToRender) {
+  const customHistory = createMemoryHistory();
+
+  return {
+    ...render(
+      <Router history={ customHistory }>
+        { componentToRender }
+      </Router>,
+    ),
+    history: customHistory,
+  }
+};
+
+export default renderWithRouter;
