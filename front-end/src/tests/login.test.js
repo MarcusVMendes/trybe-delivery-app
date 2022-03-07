9c3dfd93643a5e8e5f236b6bf3c47c5fb3368f78
Letícia Castro <aicitelks@gmail.com>
2022-03-07 17:12:53 -0300

    Código com erro foi comentado e adicionado um comentário instrutivo

--- front-end/src/tests/login.test.js
+++ front-end/src/tests/login.test.js
@@ -35,41 +35,41 @@ describe('1. Testa o componente <Login /> ', () => {
     expect(buttomLogin.disabled).toBe(true);
   });
 
-  test('Testa se é informado quando um login é inválido', () => {
+  test('Testa se aparece uma mensagem de erro quando um login é inválido', () => {
     const loginInvalid = screen.getByTestId(loginInvalidTestID);
     expect(loginInvalid.hidden).toBe(true);
 
-    // captura o input
     const emailInput = screen.getByLabelText(/login/i);
-    const passInput = screen.getByLabelText(/senha/i);
-    // escreve
     userEvent.type(emailInput, 'email@mail.com');
+
+    const passInput = screen.getByLabelText(/senha/i);
     userEvent.type(passInput, '123456');
-    // verifica
+
     const buttomLogin = screen.getByRole('button', { name: /login/i });
     userEvent.click(buttomLogin);
 
+    // DISCUTIR COM O TIME O PORQUÊ DO TESTE NÃO CONSEGUIR CAPTURAR ESSE ALERT.
     const alertMessage = screen.getByText(/Email ou senha incorretos/i)
     expect(alertMessage.textContent).toBe('Email ou senha incorretos');
   });
-
-  // test('Testa se é possível realizar o login', () => {
-  //   // captura o input
-  //   const emailInput = screen.getByLabelText(/login/i);
-  //   const passInput = screen.getByLabelText(/senha/i);
-  //   // escreve
-  //   userEvent.type(emailInput, 'zebirita@email.com');
-  //   userEvent.type(passInput, '$#zebirita#$');
-  //   // clica em login
-  //   const buttomLogin = screen.getByRole('button', { name: /login/i });
-  //   userEvent.click(buttomLogin);
-
-  //   const loading = screen.getByText(/carregando/i);
-  //   expect(loading).toBeInTheDocument();
-  // });
 });
 
-// describe('conjunto de testes 2', () => {
-//   test('', () => {});
-//   test('', () => {});
+// describe('1.1 Testa se é possível realizar o login', () => {
+//   test('Testa se ao informar email e senha corretos, se é redirecionado para a página de produtos', () => {
+//     const { history } = renderWithRouter(<Login />);
+//     // captura o input
+//     const emailInput = screen.getByLabelText(/login/i);
+//     const passInput = screen.getByLabelText(/senha/i);
+//     // escreve
+//     userEvent.type(emailInput, 'zebirita@email.com');
+//     userEvent.type(passInput, '$#zebirita#$');
+//     // clica em login
+//     const buttomLogin = screen.getByRole('button', { name: /login/i });
+//     userEvent.click(buttomLogin);
+
+//     const { pathname } = history.entries[1];
+//     expect(pathname).toMatch('/customer/products');
+//     // const loading = screen.getByText(/carregando/i);
+//     // expect(loading).toBeInTheDocument();
+//   });
 // });
