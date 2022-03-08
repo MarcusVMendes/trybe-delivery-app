132521475d2311e890f9d356016a42ecefa8800b
Letícia Castro <aicitelks@gmail.com>
2022-03-08 18:10:53 -0300

    Refatora as variáveis e cria novas.

--- front-end/src/tests/register.test.js
+++ front-end/src/tests/register.test.js
@@ -6,33 +6,38 @@ import renderWithRouter from './utils/renderWithRouter';
 import Register from '../pages/register/Register';
 
 describe('Testa o componente de página <Register />', () => {
-  let nameInput = '';
-  let emailInput = '';
-  let passwordInput = '';
+  let NAME_INPUT = '';
+  let EMAIL_INPUT = '';
+  let PASSWORD_INPUT = '';
+
+  const NAME_VALID_MSG = 'O campo nome deve ter no mínimo 12 caracteres';
+  const EMAIL_VALID_MSG = 'Digite um email válido';
+  const PASS_VALID_MSG = 'O campo senha deve ter no mínimo 6 caracteres';
 
   beforeEach(() => {
     renderWithRouter(<Register />);
 
-    nameInput = screen.getByTestId(/input-name/i);
-    emailInput = screen.getByTestId(/input-email/i);
-    passwordInput = screen.getByTestId(/input-password/i);
+    NAME_INPUT = screen.getByTestId(/input-name/i);
+    EMAIL_INPUT = screen.getByTestId(/input-email/i);
+    PASSWORD_INPUT = screen.getByTestId(/input-password/i);
   });
 
   test('Testa se são apresentados todos os campos para realizar o registro', () => {
-    expect(nameInput).toBeInTheDocument();
-    expect(emailInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
+    expect(NAME_INPUT).toBeInTheDocument();
+    expect(EMAIL_INPUT).toBeInTheDocument();
+    expect(PASSWORD_INPUT).toBeInTheDocument();
   });
 
   test('Testa se no campo NOME aparece a mensagem informativa personalizada', () => {
-    // const nameInput = screen.getByLabelText(/nome/i);
-    userEvent.type(nameInput, 'leticia');
+    userEvent.type(NAME_INPUT, 'leticia');
 
-    const alertName = screen.getByText("O campo nome deve ter no mínimo 12 caracteres");
+    const alertName = screen.getByText(NAME_VALID_MSG);
     expect(alertName).toBeInTheDocument();
   });
 
-  // test('Testa se no campo EMAIL aparece a mensagem informativa personalizada', () => {});
+  test('Testa se no campo EMAIL aparece a mensagem informativa personalizada', () => {
+
+  });
   // test('Testa se no campo SENHA aparece a mensagem informativa personalizada', () => {});
   // test('', () => {});
 });
