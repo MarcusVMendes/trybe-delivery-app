45c787e24f1856a4adb5b088d49fc7cf884a347c
Letícia Castro <aicitelks@gmail.com>
2022-03-08 18:06:06 -0300

    Refatorada a declaração das variáveis name, email e password para a forma global, assim poderão ser reaproveitadas em outros 'test()'

--- front-end/src/tests/register.test.js
+++ front-end/src/tests/register.test.js
@@ -6,21 +6,33 @@ import renderWithRouter from './utils/renderWithRouter';
 import Register from '../pages/register/Register';
 
 describe('Testa o componente de página <Register />', () => {
+  let nameInput = '';
+  let emailInput = '';
+  let passwordInput = '';
 
   beforeEach(() => {
     renderWithRouter(<Register />);
+
+    nameInput = screen.getByTestId(/input-name/i);
+    emailInput = screen.getByTestId(/input-email/i);
+    passwordInput = screen.getByTestId(/input-password/i);
   });
 
   test('Testa se são apresentados todos os campos para realizar o registro', () => {
-    const nameInput = screen.getByTestId(/input-name/i);
-    const emailInput = screen.getByTestId(/input-email/i);
-    const passwordInput = screen.getByTestId(/input-password/i);
-
     expect(nameInput).toBeInTheDocument();
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
   });
 
+  test('Testa se no campo NOME aparece a mensagem informativa personalizada', () => {
+    // const nameInput = screen.getByLabelText(/nome/i);
+    userEvent.type(nameInput, 'leticia');
 
+    const alertName = screen.getByText("O campo nome deve ter no mínimo 12 caracteres");
+    expect(alertName).toBeInTheDocument();
+  });
+
+  // test('Testa se no campo EMAIL aparece a mensagem informativa personalizada', () => {});
+  // test('Testa se no campo SENHA aparece a mensagem informativa personalizada', () => {});
   // test('', () => {});
 });
