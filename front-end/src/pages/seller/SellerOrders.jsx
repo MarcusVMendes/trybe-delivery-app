06e08795edbcb36095e699bc0a4c344aae431185
Tatiane Chiarello <tatichiarello@gmail.com>
2022-03-10 00:01:56 +0100

    Retira mock

--- front-end/src/pages/seller/SellerOrders.jsx
+++ front-end/src/pages/seller/SellerOrders.jsx
@@ -1,44 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { sellerLinks } from '../../utils/navBarLinks';
 import NavBar from '../../components/navBar/NavBar';
 import SaleCard from '../../components/seller/SaleCard';
 
-const sales = [
-  {
-  totalPrice: 5.50,
-  deliveryAddress: "R. Sollar",
-  deliveryNumber: "8",
-  status: "Pendente",
-  sellerId: 2,
-  products: [
-      {productId: 10, quantity: 3 },
-      {productId: 9, quantity: 3 },
-      {productId: 8, quantity: 3 }
-    ]
-  },
-  {
-    totalPrice: 5.50,
-    deliveryAddress: "R. Sollar",
-    deliveryNumber: "8",
-    status: "Entregue",
-    sellerId: 2,
-    products: [
-        {productId: 10, quantity: 3 },
-        {productId: 9, quantity: 3 },
-        {productId: 8, quantity: 3 }
-      ]
-    }
-]
-
 function SellerOrders() {
-  // const [orders, setOrders] = useState([]);
+  const [sales, setSales] = useState(null);
   const user = JSON.parse(localStorage.getItem('user'));
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
         const response = await api.getOrders(user.token);
-        setOrders(response);
+        setSales(response.sales);
       } catch (error) {
         console.log(error.response);
       }
@@ -47,20 +21,18 @@ function SellerOrders() {
     fetchOrders();
   }, []);
 
-  // console.log(orders)
-
-  // if (!orders) return <p>Carregando vendas...</p>;
+  if (!sales) return <p>Carregando vendas...</p>;
 
   return (
     <>
-      {/* <NavBar userName={user.name} role={user.role} links={links} /> */}
+      <NavBar userName={user.name} role={user.role} links={sellerLinks} />
       <div>
         {sales.map((order) => (
-            <SaleCard
-              key={ order.id }
-              order={ order }
-              role={user.role} 
-            />))}
+          <SaleCard
+            key={ order.id }
+            order={ order }
+            role={user.role} 
+          />))}
       </div>
     </>
   );
