95eb3d57a6c26fc4d22d47a62ab0fab4d39ebe8d
MarcusVMendes <marcus.mendes89@gmail.com>
2022-03-10 06:35:08 -0300

    Resolve lint

--- back-end/src/api/routes/user.js
+++ back-end/src/api/routes/user.js
@@ -7,6 +7,6 @@ const {
 
 router.post('/login', getUserLoginController);
 router.post('/register', registerNewUserController);
-router.get('/:email', getUserByEmailController );
+router.get('/:email', getUserByEmailController);
 
 module.exports = router;
