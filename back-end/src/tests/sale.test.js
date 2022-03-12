3ede6a91246c760ab4e4b818e968db0605f66c66
Letícia Castro <aicitelks@gmail.com>
2022-03-12 11:31:39 -0300

    SALE: 'async' adicionado em todos os 'it()'

--- back-end/src/tests/sale.test.js
+++ back-end/src/tests/sale.test.js
@@ -3,21 +3,21 @@ const { expect } = require('chai');
 
 describe('POST /sale', () => {
   describe('Valida que não é possível criar uma venda sem estar autenticado', () => {
-    it('Será validado que não é permitido criar uma venda com token expirado ou inválido', () => {});
-    it('Será validado que não é permitido criar uma venda sem um token', () => {});
+    it('Será validado que não é permitido criar uma venda com token expirado ou inválido', async () => {});
+    it('Será validado que não é permitido criar uma venda sem um token', async () => {});
   });
 
   describe('Valida se todos os campos obrigatórios são enviados corretamente ao tentar criar uma nova venda', () => {
-    it('Será validado que não é possível criar um venda sem o campo totalPrice', () => {});
-    it('Será validado que não é possível criar um venda sem o campo deliveryAddress', () => {});
-    it('Será validado que não é possível criar um venda sem o campo deliveryNumber', () => {});
-    it('Será validado que não é possível criar um venda sem o campo status', () => {});
-    it('Será validado que não é possível criar um venda sem o campo products', () => {});
-    it('Será validado que não é possível criar um venda sem o campo sellerId', () => {});
+    it('Será validado que não é possível criar um venda sem o campo totalPrice', async () => {});
+    it('Será validado que não é possível criar um venda sem o campo deliveryAddress', async () => {});
+    it('Será validado que não é possível criar um venda sem o campo deliveryNumber', async () => {});
+    it('Será validado que não é possível criar um venda sem o campo status', async () => {});
+    it('Será validado que não é possível criar um venda sem o campo products', async () => {});
+    it('Será validado que não é possível criar um venda sem o campo sellerId', async () => {});
   });
 
   describe('Valida se é possível criar uma venda com sucesso quando todos os campos são enviados corretamente', () => {
-    it('Será validado que a venda foi criada com sucesso', () => {
+    it('Será validado que a venda foi criada com sucesso', async () => {
       // verificar se os atributos aparecem no retorno e se o status da requisição é CREATED
     });
   });
@@ -25,12 +25,12 @@ describe('POST /sale', () => {
 
 describe('GET /sale', () => {
   describe('Valida se todas as vendas são listadas', () => {
-    it('Será validado que, ao estar autenticado e acessar a rota, todas as vendas são listadas', () => {});
+    it('Será validado que, ao estar autenticado e acessar a rota, todas as vendas são listadas', async () => {});
   })
 });
 
 describe('GET /sale/:id', () => {
   describe('Valida se uma venda é listada através do seu ID', () => {
-    it('Será validado que, ao estar autenticado e acessar a rota com ID da venda, ela é listada', () => {});
+    it('Será validado que, ao estar autenticado e acessar a rota com ID da venda, ela é listada', async () => {});
   });
 });
